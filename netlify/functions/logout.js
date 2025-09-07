0c0c3567dd40e6ed937dbde158e400349f1fa60b
grahame21 <143662036+grahame21@users.noreply.github.com>
2025-09-07 11:46:21 +0930

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,17 +1,7 @@
-// netlify/functions/logout.js
 exports.handler = async () => {
-  const expired = [
-    'railops_session=',
-    'HttpOnly',
-    'Secure',
-    'Path=/',
-    'SameSite=Lax',
-    'Max-Age=0'
-  ].join('; ');
-
   return {
     statusCode: 200,
-    headers: { 'Set-Cookie': expired, 'Content-Type': 'application/json' },
+    headers: { 'Set-Cookie': 'railops_session=; HttpOnly; Secure; Path=/; SameSite=Lax; Max-Age=0', 'Content-Type':'application/json' },
     body: JSON.stringify({ ok:true })
   };
 };
