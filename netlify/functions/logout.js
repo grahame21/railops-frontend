af5bf262c3b371aa902ee4a32a728e05b4bdc0f0
grahame21 <143662036+grahame21@users.noreply.github.com>
2025-10-01 12:16:43 +0000

    Self-hosted OpenLayers + minimal working map with OSM and state zooms

--- netlify/functions/logout.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { 'Set-Cookie': 'railops_session=; HttpOnly; Secure; Path=/; SameSite=Lax; Max-Age=0', 'Content-Type':'application/json' },
-    body: JSON.stringify({ ok:true })
-  };
-};
--- netlify/functions/ping.js
+++
@@ -1,6 +0,0 @@
-// Simple text response to prove /api routing works
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'text/plain' },
-  body: 'pong'
-});
