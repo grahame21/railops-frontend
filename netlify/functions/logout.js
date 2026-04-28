a14d56f7c6faae19543eea90379eafdc56114910
grahame21 <143662036+grahame21@users.noreply.github.com>
2026-04-29 01:42:36 +1000

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,10 +1,11 @@
+const { json, clearSessionCookie } = require("./_access-lib");
+
 exports.handler = async function () {
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Set-Cookie": "railops_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0",
-    },
-    body: JSON.stringify({ ok: true }),
-  };
+  return json(
+    200,
+    { ok: true },
+    {
+      "Set-Cookie": clearSessionCookie(),
+    }
+  );
 };
